21f1f1c726eed8377e0356d8339f2b5934984f9f
Carlos Eduardo <carlos.oliveira@fiap.com.br>
2024-07-31 10:43:34 -0300

    fix: testimonials section add line-height text

--- src/app/templates/Home/testimonials/index.tsx
+++ src/app/templates/Home/testimonials/index.tsx
@@ -8,7 +8,7 @@ export function Testimonials() {
     <section className="py-24 bg-gray-50">
       <GridContainer className="text-center">
         <Image src={C.logo.image} width={140} height={40} alt={C.logo.alt} className="mx-auto" />
-        <h2 className="my-8 text-5xl font-medium text-gray-900 -tracking-[0.96px]">{C.title}</h2>
+        <h2 className="my-8 text-5xl/tight font-medium text-gray-900 -tracking-[0.96px]">{C.title}</h2>
         <div>
           <Image src={C.avatar.image} width={64} height={64} alt={C.avatar.alt} className="mx-auto mb-4" />
           <span className="block text-lg font-medium text-gray-900 mb-1">{C.name}</span>
